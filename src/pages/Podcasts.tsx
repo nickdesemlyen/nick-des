141b9cd3dc8290d3f79af74cd9544b9c1170974d
stevelev <stephenlevin@gmail.com>
2025-05-26 09:53:31 +0100

    podcast change

--- src/pages/Podcasts.tsx
+++ src/pages/Podcasts.tsx
@@ -4,7 +4,7 @@ import SectionHeading from '../components/SectionHeading';
 const Podcasts: React.FC = () => {
   const podcasts = [
     {
-      title: "TEST 3",
+      title: "TEST 5",
       episode: "Episode 500: Special Edition",
       description: "A deep dive into the evolution of comedy in cinema, from the silent era to modern blockbusters.",
       date: "February 2025",

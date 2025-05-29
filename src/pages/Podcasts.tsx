20648474d6434d9d1682b87f6347bc3440994780
nickdesemlyen <nick@empiremagazine.com>
2025-05-29 21:39:41 +0100

    Update Apple Podcasts logo

--- src/pages/Podcasts.tsx
+++ src/pages/Podcasts.tsx
@@ -89,7 +89,7 @@ const Podcasts: React.FC = () => {
                           className="flex items-center justify-center px-3 py-1.5 rounded bg-white border border-gray-300 text-black hover:bg-gray-50 text-sm"
                         >
                           <img 
-                            src="/images/podcasts/applepodcasts.png" 
+                            src="/images/podcasts/image.png" 
                             alt="Apple Podcasts" 
                             className="w-3.5 h-3.5 mr-1.5" 
                           />

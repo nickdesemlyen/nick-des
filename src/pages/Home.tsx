bed2fb7322bcfb4451a6cbaf97f4d56dada0be33
nickdesemlyen <nick@empiremagazine.com>
2025-05-30 13:04:02 +0100

    Adjust book image size on homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -76,11 +76,11 @@ const Home: React.FC = () => {
             
             {/* Books */}
             <div className="bg-white rounded-lg shadow-md p-8 text-center">
-              <div className="w-full max-w-[300px] aspect-[3/4] relative mx-auto mb-6">
+              <div className="w-full h-[400px] mx-auto mb-6 flex items-center justify-center">
                 <img 
                   src="./images/books/lastactionheroes.jpg" 
                   alt="Last Action Heroes book cover"
-                  className="absolute inset-0 w-full h-full object-cover rounded-lg shadow-lg"
+                  className="h-full w-auto rounded-lg shadow-lg"
                 />
               </div>
               <h3 className="text-xl font-bold font-serif mb-4">Books</h3>
